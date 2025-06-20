c6129ee365c5cfc82c78ae0c1284ec5a8e9ad997
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-06-20 23:04:04 +0530

    Added the html file

---
+++ 29app.js
@@ -0,0 +1,25 @@
+// synchronous nature
+
+// let a = 25;
+// console.log(a);
+// let b = 10;
+// console.log(b);
+// console.log(a + b);
+
+
+
+// asunchronous nature
+
+setTimeout(() => {
+    console.log("apna college");
+}, 2000);
+setTimeout(() => {
+    console.log("hello world");
+},2000);
+
+console.log("hello...");
+
+
+
+// hello printed & stored by JS
+// and setTimer stored by browser and when JS was free, then it printed it
